e5c9780a18681149181a9f10a507f74f6fe9e096
Ryan  Wirjadi <54874063+Ryan-Wirjadi01@users.noreply.github.com>
2023-09-27 16:25:15 -0700

    updated a little bit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,41 @@
-// JavaScript to toggle the navigation bar for small screens
-function toggleNav() {
-    const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('responsive');
-}
-  
-const menuIcon = document.createElement('div');
-menuIcon.classList.add('menu-icon');
-menuIcon.addEventListener('click', toggleNav);
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('#navbar ul li a');
 
-const nav = document.querySelector('nav');
-nav.appendChild(menuIcon);
-  
+    navLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+          const targetOffset = targetSection.offsetTop;
+          const duration = 800; // Adjust the scroll duration
+          const increment = 20; // Adjust the scroll increment
+
+          scrollToTarget(targetOffset, duration, increment);
+        }
+      });
+    });
+
+    function scrollToTarget(targetOffset, duration, increment) {
+      const start = window.pageYOffset;
+      const distance = targetOffset - start;
+      const iterations = Math.ceil(duration / increment);
+
+      let i = 0;
+      const scrollStep = () => {
+        i++;
+        const easeInOutCubic = 0.5 * (1 + Math.cos(Math.PI * (i / iterations) - 1));
+        window.scrollTo(0, start + distance * easeInOutCubic);
+
+        if (i < iterations) {
+          requestAnimationFrame(scrollStep);
+        } else {
+          window.scrollTo(0, targetOffset);
+        }
+      };
+
+      scrollStep();
+    }
+  });
